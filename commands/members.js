c17ea90a22367611565b9aa667cb9b0d180ed5e2
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-05-02 21:23:51 +0200

    init

---
+++ commands/members.js
@@ -0,0 +1,25 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    var botIcon = bot.user.displayAvatarURL;
+
+    var icon = message.guild.iconURL;
+
+    var membersEmbed = new discord.RichEmbed()
+        .setTitle("SERVER INFO")
+        .setColor("#660066")
+        .setThumbnail(icon)
+        .setDescription(`**Members**\n ${message.guild.memberCount}`)
+        .setTimestamp()
+        .setFooter('BDMN', botIcon);
+
+
+
+    return message.channel.send(membersEmbed);
+
+
+}
+
+module.exports.help = {
+    name: "members"
+}
