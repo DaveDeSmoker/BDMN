bf221441a29651df1434cb6fb55aa541012841f6
DaveDeSmoker <60197890+DaveDeSmoker@users.noreply.github.com>
2020-05-04 16:12:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,44 @@ bot.on("guildMemberAdd", member => {
 
 
 // });
+var swearWords = ["kanker","discord.gg","kkr","aids","downie","homo","kalf","hoer","slet","bitch","porno","pornhub","xnxx","porn","youtube.com"];
+
+bot.on("message", async message => {
+
+    if(message.author.bot) return;
+
+    if(message.channel.type === "dn") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    var arguments = messageArray.slice(1);
+
+
+    var commands = bot.commands.get(command.slice(prefix.length));
+
+    if(commands) commands.run(bot, message, arguments);
+
+
+    var msg = message.content.toLowerCase();
+
+   for(var i = 0; i < swearWords.length; i++){
+
+        if(msg.includes(swearWords[i])) {
+
+            message.delete();
+
+            return message.channel.send("Dont send that message!").then(msg => msg.delete(5000));
+
+        }
+
+
+
+   } 
+
     if (command === `${prefix}database`) {
         var botIcon = bot.user.displayAvatarURL;
 
